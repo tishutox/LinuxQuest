7109a70fa6f1b71fc5f0b45340756b1a46d408c5
Armand <arma.asz@proton.me>
2026-03-23 12:31:03 +0100

    feat: update styles for finder results and country picker to enhance scroll behavior and layout

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'linuxquest-v21'
+const CACHE_NAME = 'linuxquest-v22'
 const STATIC_ASSETS = [
   '/',
   '/index.html',
-  '/assets/css/styles.css?v=20260318-1',
+  '/assets/css/styles.css?v=20260323-2',
   '/assets/js/main.js',
   '/assets/img/Logo.png',
   '/assets/img/bg-image.png',
